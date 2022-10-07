157bf98d3151cac8597eaac9feea2fcbd0ecfd6e
HyeonSeong <xkdtndua15@gmail.com>
2022-10-07 18:50:13 +0900

    feat: 프로젝트 style추가 및 GlobalStyle 적용

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyle } from 'style';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -11,8 +12,9 @@ let queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
       <ReactQueryDevtools />
+      <GlobalStyle />
+      <App />
     </QueryClientProvider>
   </React.StrictMode>,
 );
