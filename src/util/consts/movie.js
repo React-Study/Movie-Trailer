0d940bc750b77da52602e594614573ebfd6d68e4
HyeonSeong <xkdtndua15@gmail.com>
2022-10-31 16:18:26 +0900

    feat: movie detail 페이지 개발
    - app에 movie detail route 추가
    - movie detail page 추가
    - movie detail export 추가
    - movie const 파일에 movie detail api 호출 함수 추가
    - route const 파일에 movie detail 경로 추가

--- src/util/consts/movie.js
+++ src/util/consts/movie.js
@@ -1,2 +1,19 @@
+import http from 'util/api/base';
+
 // MOVIE 관련 상수 적는 곳입니다.
-export const MOVIE_URL = '/movie';
+const MOVIE_API_ENDPOINT = '/movie';
+
+export const getMovieDetail = async (movieId, language) => {
+  try {
+    const res = await http.get({
+      url: `${MOVIE_API_ENDPOINT}/${movieId}`,
+      params: {
+        language: language,
+      },
+    });
+
+    return res.data;
+  } catch (e) {
+    throw new Error(e);
+  }
+};
