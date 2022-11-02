529f8144f03d7f86f1560463e260978bf4cec2bf
HyeonSeong <xkdtndua15@gmail.com>
2022-11-02 19:47:05 +0900

    feat: movie detail 페이지 포스터 부분 개발
    - movieDetailLayout을 만들고 그 안에 MovieDetail 컴포넌트 삽입하는 방식으로 개발
    - movie detail api로 받아온 정보로 포스터 제목 별점 등등 받아와서 화면에 출력

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,21 +1,96 @@
-import React, { useEffect, useState } from 'react';
-import { getMovieDetail } from 'util/consts';
-import { useParams } from 'react-router-dom';
-import Layout from 'components/common/Layout/Layout';
+import React from 'react';
+import { colors } from 'style';
+import styled from 'styled-components';
+import { POSTER_PATH_END_POINT } from 'util/consts';
 
-const MovieDetail = () => {
-  const [movieDetailData, setMovieDetailData] = useState([]);
-  const id = useParams().id;
+const MovieDetail = ({ data }) => {
+  const {
+    title,
+    release_date,
+    genres,
+    runtime,
+    vote_average,
+    tagline,
+    overview,
+    poster_path,
+  } = data;
 
-  const getMovieDetailData = async (id) => {
-    setMovieDetailData(await getMovieDetail(id, 'ko-KR'));
-  };
-
-  useEffect(() => {
-    getMovieDetailData(id);
-  }, [id]);
-
-  return <Layout>{console.info(movieDetailData)}</Layout>;
+  return (
+    <PosterDetailWrap>
+      <PosterWrap>
+        <img src={`${POSTER_PATH_END_POINT}${poster_path}`} alt="영화 포스터" />
+      </PosterWrap>
+      <DetailWrap>
+        <h2>{title}</h2>
+        <ul>
+          <li>{release_date}</li>
+          <li>{genres.map((genre) => `${genre.name}, `)}</li>
+          <li>{runtime}</li>
+        </ul>
+        <VoteAverageWrap>별점(⭐): {vote_average}</VoteAverageWrap>
+        <h3>{tagline}</h3>
+        <h4>개요</h4>
+        <p>{overview}</p>
+      </DetailWrap>
+    </PosterDetailWrap>
+  );
 };
 
 export default MovieDetail;
+
+const PosterDetailWrap = styled.div`
+  width: 80%;
+  margin: 50px auto;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const PosterWrap = styled.div`
+  width: 19%;
+  margin-right: 2%;
+
+  img {
+    width: 100%;
+    height: 450px;
+    object-fit: cover;
+  }
+`;
+
+const DetailWrap = styled.div`
+  width: 69%;
+
+  h2 {
+    font-size: 30px;
+    margin-bottom: 20px;
+  }
+  ul {
+    &::after {
+      content: '';
+      display: block;
+      clear: both;
+    }
+    li {
+      float: left;
+      margin-right: 30px;
+      margin-bottom: 15px;
+    }
+  }
+  h3 {
+    font-size: 20px;
+    font-style: italic;
+    color: ${colors.gray3};
+    margin-bottom: 15px;
+  }
+  h4 {
+    font-size: 22px;
+    margin-bottom: 15px;
+  }
+  p {
+    font: 16px/1.3 'arial';
+  }
+`;
+
+const VoteAverageWrap = styled.span`
+  display: block;
+  margin-bottom: 15px;
+`;
