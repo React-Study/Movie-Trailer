93f6f2276bcb50fd68356997c0a7969b81306d1e
7ingout <101239913+7ingout@users.noreply.github.com>
2022-11-02 11:03:00 +0900

    feat: 인물 이미지 받아오기 구현

--- src/components/CharacterDetail/CharacterDetail.jsx
+++ src/components/CharacterDetail/CharacterDetail.jsx
@@ -6,6 +6,7 @@ import { Layout } from 'components/common';
 const CharacterDetail = () => {
   const [characterDetailData, setCharacterDetailData] = useState([]);
   const id = useParams().id;
+  const getImageUrl = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2/';
 
   const getCharacterDetailData = async (id) => {
     setCharacterDetailData(await getCharacterDetail(id, 'ko-KR'));
@@ -15,7 +16,16 @@ const CharacterDetail = () => {
     getCharacterDetailData(id);
   }, [id]);
 
-  return <Layout>{console.info(characterDetailData)}</Layout>;
+  return (
+    <Layout>
+      {console.info(characterDetailData)}
+      {characterDetailData.name}
+      <img
+        src={`${getImageUrl}/${characterDetailData.profile_path}`}
+        alt="personImage"
+      />
+    </Layout>
+  );
 };
 
 export default CharacterDetail;
