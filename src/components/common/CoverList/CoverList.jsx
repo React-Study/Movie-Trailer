3c98f904d249c9ec5530a6963b1b7b83127fdf80
soeun <45187472+soeunb@users.noreply.github.com>
2022-11-05 00:51:22 +0900

    feat: CoverList 컴포넌트에 카드 정보, css 추가
    - 이미지, 별점, 날짜 추가
    - css 추가
    - IMG_URL 상수는 임시로 해놓음

--- src/components/common/CoverList/CoverList.jsx
+++ src/components/common/CoverList/CoverList.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import styled from 'styled-components';
 
 const CoverList = ({ headerTitle, data }) => {
+  const IMG_URL = 'https://image.tmdb.org/t/p/w220_and_h330_face'; // 임시
   return (
     <CoverContentWrap>
       <h2>{headerTitle}</h2>
       <CoverItemList>
         {data.results &&
-          data.results.map((e) => <CoverItem key={e.id}>{e.title}</CoverItem>)}
+          data.results.map((e) => (
+            <CoverItem key={e.id}>
+              <img src={`${IMG_URL}${e.poster_path}`} />
+              <p>
+                {e.title} <span>⭐️ {e.vote_average}</span>
+              </p>
+              <span>{e.release_date}</span>
+            </CoverItem>
+          ))}
       </CoverItemList>
     </CoverContentWrap>
   );
@@ -21,6 +30,8 @@ const CoverContentWrap = styled.section`
 
   h2 {
     padding: 20px;
+    font-weight: 700;
+    font-size: 1.5em;
   }
 `;
 
@@ -32,7 +43,21 @@ const CoverItemList = styled.div`
 `;
 
 const CoverItem = styled.div`
-  margin-left: 40px;
+  margin-left: 20px;
   width: 150px;
   min-width: 150px;
+  line-height: 20px;
+
+  img {
+    width: 100%;
+    border-radius: 10px;
+  }
+
+  p {
+    font-weight: 700;
+  }
+
+  span {
+    color: rgb(0 0 0 / 60%);
+  }
 `;
