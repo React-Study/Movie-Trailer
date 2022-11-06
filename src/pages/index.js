7eb7f4eeb0f165569243b456aedc03b5fe06df95
star-think <qaws2921@hanmail.net>
2022-11-06 10:30:40 +0900

    refactor: movie-route-update movie 라우트 내용 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as HomePage } from './HomePage';
 export { default as MovieDetailPage } from './MovieDetailPage';
 export { default as CharacterListPage } from './CharacterListPage';
 export { default as CharacterDetailPage } from './CharacterDetailPage';
+export { default as MoviePage } from './MoviePage';
