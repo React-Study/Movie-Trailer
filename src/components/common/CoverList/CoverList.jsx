6d404db1d0aee51bb3b1c12da043539b31b7a963
soeun <45187472+soeunb@users.noreply.github.com>
2022-11-07 23:38:13 +0900

    feat: CoverList 컴포넌트에 tab 추가
    - CoverList: tab list, css 추가
    - Home 파일에 tab 클릭 시, 해당 category, tab으로 api 호출하는 로직 추가
    - const: home 파일에 영어->한글로 리턴해주는 로직 추가

--- src/components/common/CoverList/CoverList.jsx
+++ src/components/common/CoverList/CoverList.jsx
@@ -1,48 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { typeText } from 'util/consts';
 
-const CoverList = ({ headerTitle, data }) => {
+const CoverList = ({ headerTitle, data, types, changeTab, category }) => {
   const IMG_URL = 'https://image.tmdb.org/t/p/w220_and_h330_face'; // 임시
+  const [active, setActive] = useState(types[0]);
+
   return (
-    <CoverContentWrap>
-      <h2>{headerTitle}</h2>
-      <CoverItemList>
+    <CoverListWrap>
+      <TitleWrap>
+        <span>{headerTitle}</span>
+        <TabList>
+          {types.map((type) => (
+            <Tab
+              key={type}
+              active={active === type}
+              onClick={() => {
+                setActive(type);
+                changeTab(type, category);
+              }}
+            >
+              {typeText(type)}
+            </Tab>
+          ))}
+        </TabList>
+      </TitleWrap>
+      <ItemList>
         {data.results &&
           data.results.map((e) => (
-            <CoverItem key={e.id}>
+            <Item key={e.id}>
               <img src={`${IMG_URL}${e.poster_path}`} />
               <p>
-                {e.title} <span>⭐️ {e.vote_average}</span>
+                {e.title ? e.title : e.name} <span>⭐️ {e.vote_average}</span>
               </p>
-              <span>{e.release_date}</span>
-            </CoverItem>
+              <span>{e.release_date ? e.release_date : e.first_air_date}</span>
+            </Item>
           ))}
-      </CoverItemList>
-    </CoverContentWrap>
+      </ItemList>
+    </CoverListWrap>
   );
 };
 
 export default CoverList;
 
-const CoverContentWrap = styled.section`
+const CoverListWrap = styled.section`
   width: 80%;
   padding: 40px;
+`;
 
-  h2 {
-    padding: 20px;
+const TitleWrap = styled.div`
+  display: flex;
+  padding: 20px;
+
+  span {
     font-weight: 700;
     font-size: 1.5em;
   }
 `;
 
-const CoverItemList = styled.div`
+const TabList = styled.div`
+  border: 1px solid gray;
+  border-radius: 30px;
+  margin-top: -5px;
+  margin-left: 20px;
+`;
+
+const Tab = styled.button`
+  color: #565655;
+  font-size: 1em;
+  padding: 5px 10px 5px 10px;
+  border-radius: 30px;
+  cursor: pointer;
+  background: white;
+  border: 0;
+  outline: 0;
+  line-height: 1.5;
+  ${({ active }) =>
+    active &&
+    `
+    color: white;
+    background-color: #565655;
+  `}
+`;
+
+const ItemList = styled.div`
   display: flex;
   overflow-x: auto;
   overflow-y: hidden;
   scroll-snap-type: x proximity;
 `;
 
-const CoverItem = styled.div`
+const Item = styled.div`
   margin-left: 20px;
   width: 150px;
   min-width: 150px;
