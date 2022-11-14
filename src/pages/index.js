4f5910a6c1e63ecf16ad0ca9ac42c2ab8fc33918
soeun <45187472+soeunb@users.noreply.github.com>
2022-11-14 15:53:46 +0900

    feat: Search Page 추가
    - SearchPage: component, page, const 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 export { default as HomePage } from './HomePage';
+export { default as SearchPage } from './SearchPage';
 export { default as MovieDetailPage } from './MovieDetailPage';
 export { default as CharacterPage } from './CharacterPage';
 export { default as CharacterDetailPage } from './CharacterDetailPage';
--- src/util/consts/index.js
+++ src/util/consts/index.js
@@ -1,6 +1,7 @@
 export * from './net';
 export * from './header';
 export * from './home';
+export * from './search';
 export * from './movie';
 export * from './route';
 export * from './image';
